0b850d197069f46dac53e1e965c9d5e9235dc739
Chris Vesters <chris.vesters@outlook.com>
2025-03-14 09:37:35 +0100

    23 - Add tabs to election page.

--- frontend/src/routes/elections/[index]/+page.svelte
+++ frontend/src/routes/elections/[index]/+page.svelte
@@ -5,14 +5,42 @@
 	import CandidateTable from "$lib/candidate/CandidateTable.svelte";
 
 	const { data } = $props();
+
+	let tab: string = $state("candidates");
+
+	function openTab(tabName: string): void {
+		tab = tabName;
+	}
 </script>
 
 <h1>{@html data.info.topic}</h1>
-<h2>{$t("common.candidates")}</h2>
 
-<CandidateTable
-	electionId={data.info.id}
-	candidates={data.candidates}
-	onChange={invalidateAll}
-	onAdd={invalidateAll}
-/>
+<div class="tab">
+	<button
+		class={tab == "candidates" ? "active" : ""}
+		onclick={() => openTab("candidates")}
+	>
+		{$t("common.candidates")}
+	</button>
+	<button
+		class={tab == "votes" ? "active" : ""}
+		onclick={() => openTab("votes")}
+	>
+		{$t("common.votes")}
+	</button>
+</div>
+
+<div class="tabcontent">
+	{#if tab == "candidates"}
+		<h2>{$t("common.candidates")}</h2>
+		<CandidateTable
+			electionId={data.info.id}
+			candidates={data.candidates}
+			onChange={invalidateAll}
+			onAdd={invalidateAll}
+		/>
+	{:else if tab === "votes"}
+		<h2>{$t("common.votes")}</h2>
+		<p>votes</p>
+	{/if}
+</div>
