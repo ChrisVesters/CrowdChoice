e9e2ee860c0bb43519f4e23c2d8609bd38058f14
Chris Vesters <chris.vesters@outlook.com>
2025-03-26 08:23:19 +0100

    22 - Add election start and end information to front-end.
    Closes #22

--- frontend/src/lib/election/ElectionTypes.ts
+++ frontend/src/lib/election/ElectionTypes.ts
@@ -2,9 +2,13 @@ export type Election = {
 	id: number;
 	topic: string;
 	description: string;
+	startedOn: string | null;
+	endedOn: string | null;
 };
 
 export type CreateElectionRequest = {
 	topic: string;
 	description: string;
+	startedOn: string | null;
+	endedOn: string | null;
 };
