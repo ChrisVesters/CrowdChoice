b4cb51c05782b438bc5844b31e97da77471eab36
Chris Vesters <chris.vesters@outlook.com>
2025-04-23 07:49:30 +0200

    27 - Add election time information to vote page.
    Closes #27

--- frontend/src/routes/vote/+page.svelte
+++ frontend/src/routes/vote/+page.svelte
@@ -5,6 +5,14 @@
 </script>
 
 <h1>{@html data.election.topic}</h1>
+<div>
+	{@html data.election.startedOn
+		? new Date(data.election.startedOn).toLocaleString()
+		: ""} - {@html data.election.endedOn
+		? new Date(data.election.endedOn).toLocaleString()
+		: ""}
+</div>
 <div>{@html data.election.description}</div>
+
 
 <VoteForm electionId={data.election.id} candidates={data.candidates} />
